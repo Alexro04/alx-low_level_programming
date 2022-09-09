0988bd76ad0576075b14a6fb276aa29ea12097c3
Oluwatomi <ajakatomi@gmail.com>
2022-09-09 03:59:29 -0700

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,6 @@
-#!/bin/bash
 #include <stdio.h>
-int main(){
-	
-	char Mystr[] = "Programming is like building a multilingual puzzle"
-	puts(Mystr);
-	return 0;
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle"\n);
+	return (0);
 }
-
