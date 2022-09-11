c83c99222c4eb54e35b49d970bf3d3da663f9afd
Oluwatomi <ajakatomi@gmail.com>
2022-09-11 15:29:20 -0700

    Quotes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
  * main - entry point
  *
- * Return: Always 0
+ * Return: 1 (Error)
  */
 int main(void)
 {
-
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
+	return (1);
+}
