d0c367a10054af134f31a15edee0067644c0386d
Oluwatomi <ajakatomi@gmail.com>
2022-09-10 20:38:04 -0700

    printf wall

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- * char - a set of characters
  * main - Entry point
+ *
  * Return: 0 after printing the function
  */
 int main(void)
@@ -9,3 +9,4 @@ int main(void)
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
+
