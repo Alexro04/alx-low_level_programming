b34c8f74bd0d1da716455af03c513cd6d8b35328
Oluwatomi <ajakatomi@gmail.com>
2022-09-13 12:50:36 -0700

    0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * main - entry point
+ *
+ * Return: 0
+ *
+ * calls the _putchar function from the included main header
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
