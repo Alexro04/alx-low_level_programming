b59f2e1ab5b03ef7c963ae7c0a560a0c24919ab3
Oluwatomi <ajakatomi@gmail.com>
2022-09-16 02:15:13 -0700

    Most numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_most_numbers - prints all numbers except 2 and 4
+ *
+ * Return: 0
+ */
+void print_most_numbers(void)
+{
+	int num;
+
+	for (num = 0; num < 10;)
+	{
+		if (num == 2 || num == 4)
+		{
+			num++;
+			continue;
+		}
+		_putchar('0' + num);
+		num ++;
+	}
+	_putchar('\n');
+}
+
+
