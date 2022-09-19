fd8e0c0e756a9ef29418b89f5cc4104c45b58ef5
Oluwatomi <ajakatomi@gmail.com>
2022-09-19 02:03:30 -0700

    Advanced task

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -14,41 +14,41 @@ int y;
 
 if (n >= 0 && n <= 15)
 {
-for (x = 0; x <= n; x++)
-{
-for (y = 0; y <= n; y++)
-{
-product = x * y;
-if (y == 0)
-{
-	_putchar('0');
-}
-else if (y != 0 && product < 10)
-{
-	_putchar(',');
-	_putchar(' ');
-	_putchar(' ');
-	_putchar(' ');
-	_putchar(product + '0');
-}
-else if (product < 100)
-{
-	_putchar(',');
-	_putchar(' ');
-	_putchar(' ');
-	_putchar(product / 10 + '0');
-	_putchar(product % 10 + '0');
-}
-else if (product > 99)
-{
-	_putchar(',');
-	_putchar(' ');
-	_putchar(((product / 10)) / 10 + '0');
-	_putchar((product / 10) % 10 + '0');
-	_putchar(product % 10 + '0');
-}
-}
-_putchar('\n');
+	for (x = 0; x <= n; x++)
+	{
+		for (y = 0; y <= n; y++)
+		{
+		product = x * y;
+		if (y == 0)
+		{
+			_putchar('0');
+		}
+		else if (y != 0 && product < 10)
+		{
+			_putchar(',');
+			_putchar(' ');
+			_putchar(' ');
+			_putchar(' ');
+			_putchar(product + '0');
+		}
+		else if (product < 100)
+		{
+			_putchar(',');
+			_putchar(' ');
+			_putchar(' ');
+			_putchar(product / 10 + '0');
+			_putchar(product % 10 + '0');
+		}
+		else if (product > 99)
+		{
+			_putchar(',');
+			_putchar(' ');
+			_putchar(((product / 10)) / 10 + '0');
+			_putchar((product / 10) % 10 + '0');
+			_putchar(product % 10 + '0');
+		}
+		}
+		_putchar('\n');
 }
 }
 }
