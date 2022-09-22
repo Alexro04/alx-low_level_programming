76c59fea105866d71eceabaaeb7436a67a9b4b45
Oluwatomi <ajakatomi@gmail.com>
2022-09-22 16:42:27 -0700

    Pointers, arrays and strings

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,24 +1,45 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
-  * print_number - we're printing some serious numbers
-  * @n: that's the number, fear it.
-  *
-  * Return: void
-  */
+ * print_number - prints number
+ * @n:integer to convert to character
+ *
+ */
+
 void print_number(int n)
 {
-	unsigned int num;
+	unsigned int abs;
+	int mult = 1;
+	unsigned int abSCount;
+	int i;
+	int c = 0;
 
+	if (n == 0)
+	{
+		_putchar('0');
+	}
 	if (n < 0)
 	{
 		_putchar('-');
-		num = -n;
+		n += 1;
+		n *= -1;
+		n++;
 	}
-	else
-		num = n;
+	abs = n;
+	abSCount = n;
 
-	if (num / 10)
-		print_number(num / 10);
-	_putchar('0' + num % 10);
+	while (abSCount > 0)
+	{
+		abSCount /= 10;
+		c++;
+	}
+	for (i = 0; i < c - 1; i++)
+		mult *= 10;
+
+	for (i = 0; i < c; i++)
+	{
+		_putchar((abs / mult) + '0');
+		abs = abs % mult;
+		mult /= 10;
+	}
 }
