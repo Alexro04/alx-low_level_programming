8ec4199e08b300008ebd1878e8d22c38a3012490
Oluwatomi <ajakatomi@gmail.com>
2022-09-27 09:03:42 -0700

    README file added

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
  * _memcpy - copies the memory of src into dest
- * @dest: 
+ * @dest: first string
  * @src: the string to be copied
- * @n: 
+ * @n: size of memory to be copied
  * Return: pointer to dest
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
