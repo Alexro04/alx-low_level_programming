2ca89bd52f095c70c0cc6091d3b65226c3ac646a
Oluwatomi <ajakatomi@gmail.com>
2022-10-11 19:46:11 +0100

    macro

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,5 +1,12 @@
 #include <string.h>
 #include <stdio.h>
+/**
+  * main - checking to see if this prints name of file
+  *
+  * Return: 0
+  *
+  *
+  */
 #define __FILENAME__ (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__)
 
 int main(void)
--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,3 +1,5 @@
-#ifndef ABS
-#define ABS(x) (x < 0 ? -x : x)
-#endif /* ABS */
+#ifndef FUNCTION_LIKE_MACRO
+#define FUNCTION_LIKE_MACRO
+
+#define ABS(x) ((x) < 0 ? -(x) : (x))
+#endif /* FUNCTION_LIKE_MACRO */
